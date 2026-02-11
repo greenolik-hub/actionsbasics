f40eb624be468c2e12b0228b7c0b3debb7572532
egor <egorrcho@rambler.ru>
2026-02-11 14:32:45 +0300

    1.0 ver

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+VERSION = os.getenv("APP_VERSION", "1.0.0")
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Flask CI/CD App is running 🚀"
+    })
+
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy"
+    }), 200
+
+
+@app.route("/version")
+def version():
+    return jsonify({
+        "version": VERSION
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
